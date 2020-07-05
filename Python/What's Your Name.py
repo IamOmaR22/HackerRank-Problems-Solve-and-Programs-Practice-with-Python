a4c695e9732d6f4fd00576811ebe58c5da9af39e
Md. Omar Faruk <iamomar022@gmail.com>
2020-07-05 21:40:34 +0600

    What's Your Name?

---
+++ Python/What's Your Name.py
@@ -0,0 +1,22 @@
+# Solution - 1
+
+def print_full_name(a, b):
+    print("Hello" + ' ' + a, b + "! You just delved into python.")
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
+
+
+
+# Solution - 2
+
+def print_full_name(a, b):
+    print(f"Hello {a} {b}! You just delved into python.")
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
+
