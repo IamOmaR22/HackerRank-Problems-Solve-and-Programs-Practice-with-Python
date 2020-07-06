848c1b433190f87d75f696c2bd518ddec804626a
Md. Omar Faruk <iamomar022@gmail.com>
2020-07-06 22:15:53 +0600

    Mutations

---
+++ Python/Mutations.py
@@ -0,0 +1,26 @@
+# Solution - 1
+
+def mutate_string(string, position, character):
+    a=list(string)
+    a[position]= character
+    string=''.join(a)
+    return string
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
+
+
+
+# Solution - 2
+
+def mutate_string(string, position, character):
+    return string[:position] + character + string[position + 1:]
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
