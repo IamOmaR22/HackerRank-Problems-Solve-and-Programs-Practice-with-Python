a7dacd03eebf455f62a8c277df29a7b902ad80ba
Md. Omar Faruk <iamomar022@gmail.com>
2020-07-08 20:44:40 +0600

    Find a string

---
+++ Python/Find a string.py
@@ -0,0 +1,17 @@
+def count_substring(string, sub_string):
+    res = 0
+    len_sub = len(sub_string)
+    
+    for i in range(len(string) - len_sub + 1):
+        if string[i:i + len_sub] == sub_string:
+            res += 1
+        i += 1
+        
+    return res
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
