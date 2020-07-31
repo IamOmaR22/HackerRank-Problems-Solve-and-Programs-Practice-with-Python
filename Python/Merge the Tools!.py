60d5ba3e27345fc07d9ba9ed5a4d018a1edadf99
Md. Omar Faruk <iamomar022@gmail.com>
2020-07-31 09:26:57 +0600

    Merge the Tools!

---
+++ Python/Merge the Tools!.py
@@ -0,0 +1,13 @@
+def merge_the_tools(string, k):
+    # your code goes here
+    l = []
+    m = 0
+    for i in range(len(string)//k):
+        l.append(string[m:m+k])
+        m+=k
+    for v in l:
+        print(''.join(list(dict.fromkeys(list(v)).keys())))
+
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
