599774561c9d20037ee6d04837990a8c54126aff
Md. Omar Faruk <iamomar022@gmail.com>
2020-09-21 20:35:42 +0600

    Collections.OrderedDict()

---
+++ Python/Collections.OrderedDict().py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+from collections import OrderedDict
+
+order = OrderedDict()
+for _ in range(int(input())):
+    item, space, price = input().rpartition(' ')
+    order[item] = order.get(item, 0) + int(price)
+for item, price in order.items():
+    print(item, price)
